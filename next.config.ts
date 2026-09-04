7577cf107353e191f38b288160a8fd03c37c74b6
Rayla Matias <raylamatias09@gmail.com>
2026-09-04 14:01:50 -0300

    Atualizações nas configurações

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  webpack: (config, { dev }) => {
+    if (dev) {
+      config.watchOptions = {
+        poll: 1000,          // Checa por alterações a cada 1 segundo (1000ms)
+        aggregateTimeout: 300, // Aguarda 300ms após a alteração antes de recompilar
+      };
+    }
+    return config;
+  },
 };
-
+module.exports = nextConfig;
 export default nextConfig;
